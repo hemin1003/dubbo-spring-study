d7d503b94472d0563bc18872afcc10b7e941188c
贺敏 <hemin_it@163.com>
2017-03-22 18:30:46 +0800

    update

--- dubbo-consumer/src/main/java/com/minbo/dubbo/provider/DemoService.java
+++
@@ -1,8 +0,0 @@
-package com.minbo.dubbo.provider;
-
-/**
- * 测试远程调用的接口
- */
-public interface DemoService {
-	String sayHello(String name);
-}
--- dubbo-provider/src/main/java/com/minbo/dubbo/provider/DemoServiceImpl.java
+++ dubbo-provider/src/main/java/com/minbo/dubbo/provider/DemoServiceImpl.java
@@ -2,7 +2,6 @@ package com.minbo.dubbo.provider;
 
 public class DemoServiceImpl implements DemoService{
 
-	@Override
 	public String sayHello(String name) {
 		return "Welcome to Minbo's Dubbo, Hello " + name;
 	}
